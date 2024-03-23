fecb22fae24d32d4404aa1ca1b5eee5f93bac6c9
ruizhang <kwf8hq@virginia.edu>
2024-03-23 15:39:33 -0400

    sqlite

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db',
-        'USER': 'postgres',
-        'PASSWORD': '039210',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
