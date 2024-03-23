c4e070cd7801145b88f2c9fd517445b0c9f67148
akira <zup9su@virginia.edu>
2024-03-23 17:50:21 -0400

    making pages

--- mysite/healthyhoos/urls.py
+++ mysite/healthyhoos/urls.py
@@ -5,5 +5,10 @@ from django.urls import path
 
 app_name = "healthyhoos"
 urlpatterns = [
-    path("", views.indexview, name="index"),
+    path("", views.welcome_view, name="index"),
+    path("home/", views.home_view, name="home"),
+    path("nutrition/", views.nutrition_view, name="nutrition"),
+    path("physical-health/", views.physical_view, name="physical-health"),
+    path("mental-health/", views.mental_view, name="mental-health"),
+    path("about/", views.about_view, name="about"),
 ]
--- mysite/healthyhoos/views.py
+++ mysite/healthyhoos/views.py
@@ -3,7 +3,25 @@ from django.shortcuts import render
 # Create your views here.
 
 
+def welcome_view(request):
+    return render(request, 'healthyhoos/welcome.html')
 
-def indexview(request): 
-    return render(request, 'healthyhoos/base.html')
 
+def home_view(request):
+    return render(request, 'healthyhoos/home.html')
+
+
+def nutrition_view(request):
+    return render(request, 'healthyhoos/nutrition.html')
+
+
+def physical_view(request):
+    return render(request, 'healthyhoos/physical.html')
+
+
+def mental_view(request):
+    return render(request, 'healthyhoos/mental.html')
+
+
+def about_view(request):
+    return render(request, 'healthyhoos/about.html')
